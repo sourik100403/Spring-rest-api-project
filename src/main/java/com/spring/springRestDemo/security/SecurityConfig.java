0a195eb2bd75a3e2b16d7840b2fb3814206999d8
sourik100403 <sourikparui100403@gmail.com>
2025-05-18 13:24:06 +0530

    add private.pem and public.pem and add spring security with key

--- src/main/java/com/spring/springRestDemo/security/SecurityConfig.java
+++ src/main/java/com/spring/springRestDemo/security/SecurityConfig.java
@@ -2,18 +2,34 @@ package com.spring.springRestDemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.spring.springRestDemo.config.RsaKeyProperties;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final RsaKeyProperties rsaKeys;
+    public SecurityConfig(RsaKeyProperties rsaKeys){
+        this.rsaKeys=rsaKeys;
+    }
 
     @Bean
     public InMemoryUserDetailsManager users() {
@@ -25,6 +41,18 @@ public class SecurityConfig {
     return new InMemoryUserDetailsManager(user);
 }
 
+     @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK jwk=new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks=new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -33,6 +61,12 @@ public class SecurityConfig {
                 .requestMatchers("/test").authenticated()
                 .requestMatchers("/", "/token", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().permitAll()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> {}) // Enables JWT-based resource server
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No session
             )
             .httpBasic(Customizer.withDefaults()) // replaces .httpBasic()
             .csrf(csrf -> csrf.disable()); // replaces .csrf().disable()
